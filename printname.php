f8e9689fbffdd493a285ae5fa8ec7ec99f33667b
kegenrodrigues <kegenrodrigues95@gmail.com>
2017-01-15 17:56:17 +0530

    TheInitialCommit

---
+++ printname.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+	<title>This is PHP</title>
+</head>
+
+<body>
+
+<?php
+
+echo "Hello"." ".$_POST["firstname"]." ".$_POST["lastname"];
+echo "Your Username is.. and your Password is".$_POST["password"]
+
+?>
+
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,23 @@
+function makeid(){
+var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+	
+
+};
+
+/*
+document.write("This is working if this is the only thing")
+
+
+ $( document ).ready(function() {
+      $( "a" ).click(function( event ) {
+          alert( "The link will no longer take you to jquery.com" );
+         event.preventDefault();
+     });
+});
+*/	
